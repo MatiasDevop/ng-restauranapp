a9b798fce65805d856f7b31a8c0aa387ac8e9fcb
Nestor Matias <solomatias321@gmail.com>
2022-03-07 16:48:32 +0100

    implementing modal and forms

--- src/app/restaurant-dash/restaurant-dash.component.ts
+++ src/app/restaurant-dash/restaurant-dash.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-restaurant-dash',
   templateUrl: './restaurant-dash.component.html',
-  styleUrls: ['./restaurant-dash.component.css']
+  styleUrls: ['./restaurant-dash.component.css'],
 })
 export class RestaurantDashComponent implements OnInit {
+  formValue!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.formValue = this.formBuilder.group({
+      name: [''],
+      email: [''],
+      mobile: [''],
+      address: [''],
+      services: [''],
+    });
   }
-
 }
